9b62174f55ce640af83caf4284cf43f043ada784
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2026-03-11 16:33:47 +0100

    feat(Validations): Ajout de bon mesages d'erreur
    [39][Done]

--- src/app/validators/card.ts
+++ src/app/validators/card.ts
@@ -1,20 +1,25 @@
 import vine from '@vinejs/vine'
+import db from '@adonisjs/lucid/services/db'
+
+const uniqueCard = vine.createRule(async (value, _, field) => {
+    const deckId = field.parent.deckId
+
+    const card = await db
+        .from('cards')
+        .where('question', value)
+        .where('deck_id', deckId)
+        .first()
+
+    if (card) {
+        field.report('Cette question existe déjà', field + '.unique', field)
+    }
+})
 
 const cardValidator = vine.compile(
     vine.object({
-        question: vine.string().trim().minLength(10).maxLength(500).unique(async (db, value, field) => {
-            const deckId = field.parent.deckId
+        question: vine.string().trim().minLength(10).use(uniqueCard()),
 
-            const card = await db
-                .from('cards')
-                .where('question', value)
-                .where('deck_id', deckId)
-                .first()
-
-            return !card
-        }),
-
-        reponse: vine.string().trim().minLength(1).maxLength(1000),
+        reponse: vine.string().trim().minLength(1),
 
         deckId: vine.number().positive().exists(async (db, value) => {
             const deck = await db.from('decks').where('id', value).first()
@@ -22,4 +27,5 @@ const cardValidator = vine.compile(
         }),
     })
 )
+
 export { cardValidator }
--- src/app/validators/deck.ts
+++ src/app/validators/deck.ts
@@ -1,8 +1,22 @@
 import vine from '@vinejs/vine'
+import db from '@adonisjs/lucid/services/db'
+
+const uniqueDeckTitle = vine.createRule(async (value, _, field) => {
+    const deck = await db
+        .from('decks')
+        .where('titre', value)
+        .first()
+
+    if (deck) {
+        field.report('Ce deck existe déjà', field + '.unique', field)
+    }
+})
+
 const deckValidator = vine.compile(
     vine.object({
-        titre: vine.string().trim().minLength(2).unique({ table: "decks", column: "titre" }),
-        description: vine.string().trim().minLength(5).optional(),
+        titre: vine.string().trim().minLength(3).use(uniqueDeckTitle()),
+        description: vine.string().trim().minLength(10),
     })
 )
+
 export { deckValidator }
